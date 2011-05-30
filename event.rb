d5861b83940cb843d2678aedc1adb03e597d2132
D Girish Kumar Reddy <girish.iiith@gmail.com>
2011-05-30 19:42:23 +0530

    More logical ordering of slides for eventmachine api

--- event.rb
+++ event.rb
@@ -1,5 +1,19 @@
 require 'eventmachine'
+# Basic
+EM.run do
+  EM.stop
+end
 
+# Timers
+EM.run do
+  EM.add_timer(100) do
+    puts "finished after 100 ms"
+    EM.stop
+  end
+  puts "wait.."
+end
+
+# Next Tick
 EM.run do
   EM.next_tick do
     puts 2
@@ -8,11 +22,13 @@ EM.run do
   puts 1
 end
 
-
-EM.run do
-  EM.next_tick do
-    puts 2
+#No-Loop
+EM.run  do
+  EM.add_timer(100) do
+    puts "finished after 10000 ms"
     EM.stop
   end
-  puts 1
+  1.upto(10000) do
+    # sleep 1
+  end
 end
